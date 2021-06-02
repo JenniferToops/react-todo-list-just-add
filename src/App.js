794dbcccadc8e0854b26232907bcecc18a8655c9
Jennifer Toops <jennifertoops@gmail.com>
2021-06-02 19:57:57 -0400

    started commenting on App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import { useState } from 'react'
+// imports the useState hook (React v6.18) which allows you to use state without classes
 import Header from "./components/Header";
+// imports the Header component from the components folder
 import List from "./components/List"
+// imports List component from components folder
 import AddTodo from "./components/AddTodo"
+// imports AddTodo component from components folder
 
 function App() {
+  // creates function "App" with props
 
   const [todos, setTodos] = useState([
+    //deconstructs todos and setTodos
     {
       id: 1,
+      //prop = id, value = 1
       item: "Drink Water",
+      //prop = item, value = drink water
     },
     {
       id: 2,
+      //prop = id, value = 2
       item: "Make Healthy Dinner",
+      //prop = item, value = make healthy dinner
     },
     {
       id: 3,
+      //prop = id, value = 3
       item: "Exercise",
+      //prop = item, value = exercise
     },
   ]);
 
