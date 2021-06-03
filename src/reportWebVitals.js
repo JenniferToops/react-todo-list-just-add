aca79c6a2aedae2b2822300fae6b301146e9ace1
Jennifer Toops <jennifertoops@gmail.com>
2021-06-03 17:18:40 -0400

    updated the addtodo component, indexCSS, reportWebVitals

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -9,5 +9,12 @@ const reportWebVitals = onPerfEntry => {
     });
   }
 };
+// sets up the function to get web vitals (metrics): Web Vitals are a set of useful metrics that aim to capture the user experience of a web page. The following web vitals are all included: 
+/*Time to First Byte (TTFB)
+First Contentful Paint (FCP)
+Largest Contentful Paint (LCP)
+First Input Delay (FID)
+Cumulative Layout Shift (CLS)*/
 
 export default reportWebVitals;
+// exports the reportWebVitals 
